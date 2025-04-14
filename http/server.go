088b268ac2924fa5836cb295f2751e4052843300
毛亚琛 <maoyachen55@gmail.com>
2025-04-14 17:38:50 +0800

    fix web

--- http/server.go
+++ http/server.go
@@ -117,7 +117,7 @@ func NewServer(config *common.Config) (*HTTPServer, error) {
 	server.app.Use(sessions.Sessions("sshmux", sessionStore))
 
 	gv := ginview.New(goview.Config{
-		Root:         "../http/templates",
+		Root:         "templates",
 		Extension:    ".tmpl",
 		Master:       "layout",
 		DisableCache: true,
@@ -136,7 +136,7 @@ func NewServer(config *common.Config) (*HTTPServer, error) {
 		},
 	})
 
-	// gv.SetFileHandler(embeddedFH)
+	gv.SetFileHandler(embeddedFH)
 
 	server.app.HTMLRender = gv
 
